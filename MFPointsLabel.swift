d3615479956778c7c929d4131ccad431ddccbbc8
mattcmf <mattcmf@gmail.com>
2015-10-17 09:21:23 +0100

    Added fix for IOS9 issue, added high scores, tweaked speed and added levels

---
+++ MFPointsLabel.swift
@@ -0,0 +1,9 @@
+//
+//  MFPointsLabel.swift
+//  Nimble Ninja
+//
+//  Created by Matthew Frampton on 13/10/2015.
+//  Copyright © 2015 theswiftproject. All rights reserved.
+//
+
+import Foundation
